7c18b17cc1abc470947b8a39382ead828b7e2f44
Shuichiro.TAKAHASHI <reg@shuichiro.jp>
2022-11-27 17:50:14 +0900

    graqhqlファイルを作成するところまで

--- frontend/@generated/graphql.ts
+++ frontend/@generated/graphql.ts
@@ -13,6 +13,20 @@ export type GetCalenderListQueryVariables = Exact<{
 
 export type GetCalenderListQuery = { __typename?: 'Query', CalendarList: Array<{ __typename?: 'calendar', year_month: string, day: string, workday: boolean, saturday: boolean, holiday: boolean }> };
 
+export type GetEmployeesQueryVariables = Exact<{
+  where: EmployeesWhereUniqueInput;
+}>;
+
+
+export type GetEmployeesQuery = { __typename?: 'Query', Employees?: { __typename?: 'employees', employees_id: string, employees_name: string, department_id: number } | null };
+
+export type GetOperationListQueryVariables = Exact<{
+  where: OperationWhereInput;
+}>;
+
+
+export type GetOperationListQuery = { __typename?: 'Query', OperationList: Array<{ __typename?: 'operation', operation_id: string, operation_name: string }> };
+
 
 export const GetCalenderListDocument = `
     query getCalenderList($where: calendarWhereInput!) {
@@ -41,4 +55,57 @@ export const useGetCalenderListQuery = <
     );
 
 useGetCalenderListQuery.getKey = (variables: GetCalenderListQueryVariables) => ['getCalenderList', variables];
+;
+
+export const GetEmployeesDocument = `
+    query getEmployees($where: employeesWhereUniqueInput!) {
+  Employees(where: $where) {
+    employees_id
+    employees_name
+    department_id
+  }
+}
+    `;
+export const useGetEmployeesQuery = <
+      TData = GetEmployeesQuery,
+      TError = unknown
+    >(
+      client: GraphQLClient,
+      variables: GetEmployeesQueryVariables,
+      options?: UseQueryOptions<GetEmployeesQuery, TError, TData>,
+      headers?: RequestInit['headers']
+    ) =>
+    useQuery<GetEmployeesQuery, TError, TData>(
+      ['getEmployees', variables],
+      fetcher<GetEmployeesQuery, GetEmployeesQueryVariables>(client, GetEmployeesDocument, variables, headers),
+      options
+    );
+
+useGetEmployeesQuery.getKey = (variables: GetEmployeesQueryVariables) => ['getEmployees', variables];
+;
+
+export const GetOperationListDocument = `
+    query getOperationList($where: operationWhereInput!) {
+  OperationList(where: $where) {
+    operation_id
+    operation_name
+  }
+}
+    `;
+export const useGetOperationListQuery = <
+      TData = GetOperationListQuery,
+      TError = unknown
+    >(
+      client: GraphQLClient,
+      variables: GetOperationListQueryVariables,
+      options?: UseQueryOptions<GetOperationListQuery, TError, TData>,
+      headers?: RequestInit['headers']
+    ) =>
+    useQuery<GetOperationListQuery, TError, TData>(
+      ['getOperationList', variables],
+      fetcher<GetOperationListQuery, GetOperationListQueryVariables>(client, GetOperationListDocument, variables, headers),
+      options
+    );
+
+useGetOperationListQuery.getKey = (variables: GetOperationListQueryVariables) => ['getOperationList', variables];
 ;
